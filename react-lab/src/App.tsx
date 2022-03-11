6a2f42e8cc86c0acaee34f1c6c40bcff0de13064
Naafiyan Ahmed <naafiyan@gmail.com>
2022-03-10 20:19:01 -0500

    finished

---
+++ react-lab/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Horoscope from './Horoscope';
+
+function App() {
+  return (
+    <div className="App">
+      <Horoscope />
+    </div>
+  );
+}
+
+export default App;
